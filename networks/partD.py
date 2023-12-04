136277a50e9b54121556a353aec7eab8ffa749ec
Sinclair Tse <111295409+shady1543@users.noreply.github.com>
2023-12-04 10:43:47 +0800

    update partD

--- networks/partD.py
+++ networks/partD.py
@@ -8,22 +8,18 @@ class CAMGenerator(nn.Module):
 
     Args:
     - input_size (int): The size of the input features.
-    - output_size (int): The size of the output features before classification.
-    - num_classes (int): The number of classes for classification.
+    - output_size (int): The size of the output features.
 
     Returns:
     - cam_output: The output features from the decoder.
-    - logits: The classification logits.
     '''
-    def __init__(self, input_size, output_size, num_classes):
+    def __init__(self, input_size, output_size):
         super(CAMGenerator, self).__init__()
         self.decoder = nn.Linear(input_size, output_size)
-        self.classifier = nn.Linear(output_size, num_classes)
 
     def forward(self, x):
         cam_output = self.decoder(x)
-        logits = self.classifier(cam_output)
-        return cam_output, logits
+        return cam_output
 
 class CAMGeneratorNetwork:
     '''
@@ -31,23 +27,20 @@ class CAMGeneratorNetwork:
 
     Args:
     - vit_model: A pretrained vision transformer model.
+    - classifier: A pretrained classifier.
     - input_data: The input data for the network.
     - ground_truth_labels: The ground truth labels for the input data.
-
-    The network computes CAMs and trains the CAMGenerator to optimize class activation maps and classification performance.
     '''
-    def __init__(self, vit_model, input_data, ground_truth_labels):
+    def __init__(self, vit_model, classifier, input_data, ground_truth_labels):
         self.vit_model = vit_model
+        self.classifier = classifier
         self.input_data = input_data
         self.ground_truth_labels = ground_truth_labels
 
-        # Initialize CAMGenerator
         input_size = self.vit_model.token_embeddings.size(-1)  # Size of token embeddings
         output_size = 128  # Size of CAM output
-        num_classes = 2  # Number of classes
-        self.cam_generator = CAMGenerator(input_size, output_size, num_classes)
+        self.cam_generator = CAMGenerator(input_size, output_size)
 
-        # Loss function and optimizer
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.cam_generator.parameters(), lr=0.001)
 
@@ -66,29 +59,26 @@ class CAMGeneratorNetwork:
             self.optimizer.zero_grad()
             token_embeddings = self.vit_model(self.input_data)
 
-            # Generate CAM & classification predictions
-            cam_output, logits = self.cam_generator(token_embeddings)
+            cam_output = self.cam_generator(token_embeddings)
 
-            # Compute boundary loss & classification loss
+            logits = self.classifier(cam_output)
+
             b_loss = self.boundary_loss(cam_output)
             c_loss = self.criterion(logits, self.ground_truth_labels)
             total_loss = b_loss + c_loss
 
-            # Backpropagation and optimization
             total_loss.backward()
             self.optimizer.step()
 
-            # Save CAM output for this epoch
             all_cam_outputs.append(cam_output.detach())
 
-            # Log training progress
             print(f"Epoch [{epoch + 1}/{num_epochs}], Boundary Loss: {b_loss.item()}, Classification Loss: {c_loss.item()}")
 
-        # Concatenate all CAM outputs
         final_cam_output = torch.cat(all_cam_outputs, dim=0)
 
         return final_cam_output
 
 # Example usage:
-# cam_network = CAMGeneratorNetwork(vit_model, input_data, ground_truth_labels)
+# pretrained_classifier = ...
+# cam_network = CAMGeneratorNetwork(vit_model, pretrained_classifier, input_data, ground_truth_labels)
 # final_cam_output = cam_network.train(num_epochs=10)
