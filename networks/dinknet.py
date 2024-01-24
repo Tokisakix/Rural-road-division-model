2c34e1f9a6ceedb6ad38fdc55d00a09a949a353c
Linda-JYLin <66871642+Linda-JYLin@users.noreply.github.com>
2024-01-24 11:34:19 +0800

    add DinkNet34LessPool

--- networks/dinknet.py
+++ networks/dinknet.py
@@ -113,6 +113,83 @@ class DinkNet34(nn.Module):
         
         return F.sigmoid(out)
 
+class Dblock_more_dilate(nn.Module):
+    def __init__(self, channel):
+        super(Dblock_more_dilate, self).__init__()
+        self.dilate1 = nn.Conv2d(channel, channel, kernel_size=3, dilation=1, padding=1)
+        self.dilate2 = nn.Conv2d(channel, channel, kernel_size=3, dilation=2, padding=2)
+        self.dilate3 = nn.Conv2d(channel, channel, kernel_size=3, dilation=4, padding=4)
+        self.dilate4 = nn.Conv2d(channel, channel, kernel_size=3, dilation=8, padding=8)
+        self.dilate5 = nn.Conv2d(channel, channel, kernel_size=3, dilation=16, padding=16)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+    def forward(self, x):
+        dilate1_out = ReLU(self.dilate1(x))
+        dilate2_out = ReLU(self.dilate2(dilate1_out))
+        dilate3_out = ReLU(self.dilate3(dilate2_out))
+        dilate4_out = ReLU(self.dilate4(dilate3_out))
+        dilate5_out = ReLU(self.dilate5(dilate4_out))
+        out = x + dilate1_out + dilate2_out + dilate3_out + dilate4_out + dilate5_out
+        return out
+
+
+class DinkNet34_less_pool(nn.Module):
+    def __init__(self, num_classes=1):
+        super(DinkNet34_less_pool, self).__init__()
+
+        filters = [64, 128, 256, 512]
+        resnet = models.resnet34(pretrained=True)
+
+        self.firstconv = resnet.conv1
+        self.firstbn = resnet.bn1
+        self.firstrelu = resnet.relu
+        self.firstmaxpool = resnet.maxpool
+        self.encoder1 = resnet.layer1
+        self.encoder2 = resnet.layer2
+        self.encoder3 = resnet.layer3
+
+        self.dblock = Dblock_more_dilate(256)
+
+        self.decoder3 = DecoderBlock(filters[2], filters[1])
+        self.decoder2 = DecoderBlock(filters[1], filters[0])
+        self.decoder1 = DecoderBlock(filters[0], filters[0])
+
+        self.finaldeconv1 = nn.ConvTranspose2d(filters[0], 32, 4, 2, 1)
+        self.finalrelu1 = ReLU
+        self.finalconv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.finalrelu2 = ReLU
+        self.finalconv3 = nn.Conv2d(32, num_classes, 3, padding=1)
+
+    def forward(self, x):
+        # Encoder
+        x = self.firstconv(x)
+        x = self.firstbn(x)
+        x = self.firstrelu(x)
+        x = self.firstmaxpool(x)
+        e1 = self.encoder1(x)
+        e2 = self.encoder2(e1)
+        e3 = self.encoder3(e2)
+
+        # Center
+        e3 = self.dblock(e3)
+
+        # Decoder
+        d3 = self.decoder3(e3) + e2
+        d2 = self.decoder2(d3) + e1
+        d1 = self.decoder1(d2)
+
+        # Final Classification
+        out = self.finaldeconv1(d1)
+        out = self.finalrelu1(out)
+        out = self.finalconv2(out)
+        out = self.finalrelu2(out)
+        out = self.finalconv3(out)
+
+        return F.sigmoid(out)
+
 if __name__ == "__main__":
     model   = DinkNet34()
     inputs  = torch.rand(4, 3, 1024, 1024)
